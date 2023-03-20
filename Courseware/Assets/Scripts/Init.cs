ce11b5f35a9a9aa55b6a4f8ddb09dbf3cd56c9f5
叶偲诚 <ysich@qq.com>
2023-03-20 17:54:06 +0800

    feat:init

---
+++ Courseware/Assets/Scripts/Init.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    //拼图移动时的go拖拽时保持在最上层
+    public static GameObject showGo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Courseware/Assets/Scripts/Puzzle/PuzzleItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SocialPlatforms;
+
+public class PuzzleItem: MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    public Transform orignaParent;
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        orignaParent = transform.parent;
+        Transform targetTransform = Init.showGo.transform;
+        transform.SetParent(targetTransform);
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(orignaParent);
+    }
+}
